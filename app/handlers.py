7d50d1b35a88f0d49d33edc4f6130027fbd2aeea
plov <zrimen555@gmail.com>
2026-05-16 11:14:03 +0300

    Add stiker2

--- app/handlers.py
+++ app/handlers.py
@@ -134,8 +134,8 @@ async def send_admin_panel(message: types.Message, edit=False):
     online = await req.get_online_users()
     text = (f"🛠 <b>Админ-панель</b>\n\n"
             f"👤 Всего пользователей: <code>{total}</code>\n"
-            f"🟢 В сети: <code>{online}</code>"
-            f"Ты админ ты крутой или нет?")
+            f"🟢 В сети: <code>{online}</code>\n"
+            f"☠️Ты админ ты крутой")
     kb_markup = await kb.admin_broadcast_inline_keyboard()
 
     if edit:
